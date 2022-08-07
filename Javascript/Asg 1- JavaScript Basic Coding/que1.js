c17bdaf2cfbcce864146072112276f2b56cb1a6e
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-08-07 17:13:46 +0530

    que1 of asg 1  - JS Edyoda
    basisc asg  1

---
+++ Javascript/Asg 1- JavaScript Basic Coding/que1.js
@@ -0,0 +1,15 @@
+ // Question 1:
+    // var str1 = 'Today is';
+    // var str2 = '      a beautiful day     '
+    // var str3 = ' In Hawaii.     '
+    // Result = 'Today is a beautiful day In Hawaii.'
+    // Use the str1, str2, str3 variables to create the Result string. 
+    // Keep the extra spaces, lowercase and uppercase letters in mind.
+
+    var str1 = 'Today is';
+    var str2 = '      a beautiful day     ';
+    var str3 = ' In Hawaii.     ';
+
+    var result = str1 +" "+ str2.trim() +" "+ str3.trim();
+    console.log(result); //Today is a beautiful day In Hawaii.
+   
