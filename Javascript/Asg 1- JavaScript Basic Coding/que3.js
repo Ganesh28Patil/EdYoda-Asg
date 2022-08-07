3b508b4500c36794183068d6e6561ce0770949e5
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-08-07 18:59:03 +0530

    question alloted

---
+++ Javascript/Asg 1- JavaScript Basic Coding/que3.js
@@ -0,0 +1,4 @@
+// Question 3:
+// Write a JavaScript program to create menu driven calculator that performs basic arithmetic operations (add, subtract, multiply and divide) using switch case. The calculator should input two numbers and an operator: +, -, *, / from the user. It should perform operation according to the operator entered and must take input in given format.
+
+
--- Javascript/Asg 1- JavaScript Basic Coding/que5.js
+++ Javascript/Asg 1- JavaScript Basic Coding/que5.js
@@ -1,38 +1,3 @@
-console.clear();
-
-// Question 1:
-// var str1 = 'Today is';
-// var str2 = '      a beautiful day     '
-// var str3 = ' In Hawaii.     '
-// Result = 'Today is a beautiful day In Hawaii.'
-// Use the str1, str2, str3 variables to create the Result string. Keep the extra spaces, lowercase and uppercase letters in mind.
-
-console.log("Q1 END =====================")
-
-// Question 2:
-// var enteredAlphabet = 'd' or "D"
-// Write a program to check vowel, consonant or non-alphabet using switch case. Change value of enteredAlphabet and check if your code works fine.
-
-console.log("Q2 END =====================")
-
-// Question 3:
-// Write a JavaScript program to create menu driven calculator that performs basic arithmetic operations (add, subtract, multiply and divide) using switch case. The calculator should input two numbers and an operator: +, -, *, / from the user. It should perform operation according to the operator entered and must take input in given format.
-
-
-console.log("Q3 END =====================")
-
-
-// Question 4:
-// Write a JavaScript program to input sides of a triangle and check whether a triangle is equilateral, scalene or isosceles triangle using if else.
-// Properties of triangle:
-// A triangle is said Equilateral Triangle, if all its sides are equal. If a, b, c are three sides of triangle. Then, the triangle is equilateral only if a == b == c.
-// A triangle is said Isosceles Triangle, if its two sides are equal. If a, b, c are three sides of triangle. Then, the triangle is isosceles if either a == b or a == c or b == c.
-// A triangle is said Scalene Triangle, if none of its sides are equal.
-
-
-console.log("Q4 END =====================")
-
-
 // Question 5:
 // Write a JavaScript program to input electricity unit charge and calculate the total electricity bill according to the given condition:
 // For first 50 units Rs. 0.50/unit
