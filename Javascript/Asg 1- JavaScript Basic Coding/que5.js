42b6ad99ef1322acde2b831dda57035bc34e6699
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-08-07 23:23:57 +0530

    que3 done

--- Javascript/Asg 1- JavaScript Basic Coding/que5.js
+++ Javascript/Asg 1- JavaScript Basic Coding/que5.js
@@ -8,9 +8,33 @@
 // An additional surcharge of 20% is added to the bill.
 // For example, input: 20 units then bill is Rs10
 // Total Units: 300
-// 50 * 0.5 = 25
+// 50 * 0.5 = 25  // 
 // 100 * 0.75 = 75
 // 100 * 1.2 = 120
 // 50 * 1.5 = 75
 // Total = 295 + 20% surcharge on 295
 // Result = INR 354
+
+let units = 300;
+let bill = 0;
+if(units<=50){
+  bill  = units*0.5;
+}
+else if(units <= 150){
+  bill = 50*0.5 + (units-50)*0.75;
+}
+else if(units <= 250){
+  bill = 50*0.5 + 100*0.75 + (units-150)*1.2;
+}
+else if(units > 250){
+  bill = 50*0.5 + 100*0.75 + 150*1.2 + (units-250)*1.5;
+}
+
+if(bill>150){
+  bill = bill + bill*0.2;
+}
+
+console.log(bill);
+
+
+
