2d2a3bfb2fc93418924f6ee34fdc317c9c280d3f
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-08-07 19:04:42 +0530

    question framed

--- Javascript/Asg 1- JavaScript Basic Coding/que5.js
+++ Javascript/Asg 1- JavaScript Basic Coding/que5.js
@@ -1,5 +1,6 @@
 // Question 5:
-// Write a JavaScript program to input electricity unit charge and calculate the total electricity bill according to the given condition:
+// Write a JavaScript program to input electricity unit charge and 
+// calculate the total electricity bill according to the given condition:
 // For first 50 units Rs. 0.50/unit
 // For next 100 units Rs. 0.75/unit
 // For next 100 units Rs. 1.20/unit
@@ -12,5 +13,3 @@
 // 50 * 1.5 = 75
 // Total = 295 + 20% surcharge on 295
 // Result = INR 354
-
-console.log("Q5 END =====================")
